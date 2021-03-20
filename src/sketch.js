7db0e642a238c3e5e804b7d894b699904ce69652
Sricharan Ramesh <charan1952001@gmail.com>
2021-03-20 23:47:50 +0530

    Added neural network

--- src/sketch.js
+++ src/sketch.js
@@ -1,47 +1,40 @@
-import React from "react";
-import "./App.css";
-import * as ml5 from 'ml5'
-import Sketch from "react-p5";
-import classification_model from './model3/model.json'
-import classification_model_meta from './model3/model_meta.json'
+let video;
+let poseNet;
+let pose;
+let skeleton;
 
-function App() {
+let brain;
+let poseLabel = "Y";
 
-  let video;
-  let poseNet;
-  let pose;
-  let skeleton;
-
-  let brain;
-  let poseLabel = "Y";
-
-  const setup = (p5, canvasParentRef) => {
-    p5.createCanvas(640, 480).parent(canvasParentRef);
-    video = p5.createCapture(p5.VIDEO);
+export default function sketch(p) {
+  
+  p.setup = function () {
+    p.createCanvas(640, 480);
+    video = p.createCapture(p.VIDEO);
     video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
-  
+
     let options = {
       inputs: 34,
-      outputs: 5,
+      outputs: 4,
       task: 'classification',
       debug: true
     }
     brain = ml5.neuralNetwork(options);
     const modelInfo = {
-      model: 'model3/model.json',
-      metadata: 'model3/model_meta.json',
-      weights: 'model3/model.weights.bin',
+      model: './model3/model.json',
+      metadata: './model3/model_meta.json',
+      weights: './model3/model.weights.bin',
     };
     brain.load(modelInfo, brainLoaded);
   }
-    
+
   function brainLoaded() {
     console.log('pose classification ready!');
     classifyPose();
   }
-  
+
   function classifyPose() {
     if (pose) {
       let inputs = [];
@@ -56,65 +49,56 @@ function App() {
       setTimeout(classifyPose, 100);
     }
   }
-  
+
   function gotResult(error, results) {
-    console.log(results[0].label)
+    
     if (results[0].confidence > 0.75) {
       poseLabel = results[0].label.toUpperCase();
     }
     //console.log(results[0].confidence);
     classifyPose();
   }
-  
-  
+
   function gotPoses(poses) {
     if (poses.length > 0) {
       pose = poses[0].pose;
       skeleton = poses[0].skeleton;
     }
   }
-  
+
   function modelLoaded() {
     console.log('poseNet ready');
   }
-  
-  function draw(p5) {
-    p5.push();
-    p5.translate(video.width, 0);
-    p5.scale(-1, 1);
-    p5.image(video, 0, 0, video.width, video.height);
-  
+
+  p.draw = function() {
+    p.push();
+    p.translate(video.width, 0);
+    p.scale(-1, 1);
+    p.image(video, 0, 0, video.width, video.height);
+
     if (pose) {
       for (let i = 0; i < skeleton.length; i++) {
         let a = skeleton[i][0];
         let b = skeleton[i][1];
-        p5.strokeWeight(2);
-        p5.stroke(0);
-  
-        p5.line(a.position.x, a.position.y, b.position.x, b.position.y);
+        p.strokeWeight(2);
+        p.stroke(0);
+
+        p.line(a.position.x, a.position.y, b.position.x, b.position.y);
       }
       for (let i = 0; i < pose.keypoints.length; i++) {
         let x = pose.keypoints[i].position.x;
         let y = pose.keypoints[i].position.y;
-        p5.fill(0);
-        p5.stroke(255);
-        p5.ellipse(x, y, 16, 16);
+        p.fill(0);
+        p.stroke(255);
+        p.ellipse(x, y, 16, 16);
       }
     }
-    p5.pop();
-  
-    p5.fill(255, 0, 255);
-    p5.noStroke();
-    p5.textSize(512);
-    p5.textAlign(p5.CENTER, p5.CENTER);
-    p5.text(poseLabel, 320, 240);
+    p.pop();
+
+    p.fill(255, 0, 255);
+    p.noStroke();
+    p.textSize(512);
+    p.textAlign(p.CENTER, p.CENTER);
+    p.text(poseLabel, video.width / 2, video.height / 2);
   }
-
-  return (
-    <div className="App" style={{ height: '100vh', display: "flex", justifyContent: 'center', alignItems: 'center' }}>
-      <Sketch setup={setup} draw={draw} />
-    </div>
-  );
 }
-
-export default App;
